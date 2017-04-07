04e9a0f73db1df831b9f70994e07041f82fe5218
Balabusko Viktor <pravosudor@gmail.com>
2017-04-07 15:33:47 +0400

    p

--- src/by/htp/punktProkata/logica/Print.java
+++ src/by/htp/punktProkata/logica/Print.java
@@ -1,8 +1,16 @@
 package by.htp.punktProkata.logica;
 
+import by.htp.punktProkata.domen.equipment.*;
+
 public class Print {
 	public static void maxSize() {
 		System.out.println("bolshe brat nelzya");
+	}
+	
+	public static void showEquipment(Equipment[] listEquipment) {
+		for(Equipment i : listEquipment) {
+			System.out.println(i);
+		}
 	}
 
 }
